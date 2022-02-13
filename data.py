21dee83f26d83781da340e686c7dcdc6ed221ee1
Ved Suthar <vedhsuthar@gmail.com>
2022-02-13 21:04:12 +0530

    Finished image gathering process

--- data.py
+++ data.py
@@ -7,20 +7,37 @@ Created on Sun Feb 13 16:29:52 2022
 
 #Imports
 import numpy as np
-
+from PIL import Image
+from os import walk
 
 #The points which are used for the data
-points=['Left Eyebrow Left','Left Eyebrow Center','Left Eyebrow Right','Right Eyebrow Left','Right Eyebrow Center','Right Eyebrow Right','Left Eye Left','Left Eye Top','Left Eye Pupil','Left Eye Bottom','Left Eye Right','Right Eye Left','Right Eye Top','Right Eye Pupil','Right Eye Bottom','Right Eye Right','Nose Top','Nose Tip','Mouth Left Tip','Mouth Left Center','Mouth Center','Mouth Right Center','Mouth Right Tip','Chin']
+points=['Left Eyebrow Left','Left Eyebrow Center','Left Eyebrow Right','Right Eyebrow Left','Right Eyebrow Center','Right Eyebrow Right','Left Eye Left','Left Eye Top','Left Eye Pupil','Left Eye Bottom','Left Eye Right','Right Eye Left','Right Eye Top','Right Eye Pupil','Right Eye Bottom','Right Eye Right','Nose Top','Nose Tip','Mouth Left Tip','Upper Lip Left Center','Upper Lip Center','Upper Lip Right Center','Mouth Right Tip','Lower Lip Left Center','Lower Lip Center','Lower Lip Right Center','Chin']
 
 
 #data class which makes the raw format of the data
 class data:
     img=None
-    location=np.zeros((len(points)))
+    location=np.zeros((len(points),2),dtype=np.uint32)
+    
+    def save(self,path='Data/raw/'):
+        #getting new id
+        gid=0
+                
+        filenames=next(walk(path), (None, None, []))[2]
+        if len(filenames)>0:
+            filename=filenames[-1]        
+            gid=int(filename[0:filename.index('.')])+1
+            
+        
+        #Save with id
+        im = Image.fromarray(self.img)
+        im.save(path+str(gid)+".jpeg")
+        np.save(path+str(gid)+".npy",self.location)
+        
     
     
 #Main class to test    
 if __name__ == '__main__':
     d = data()
-    print(d.location.shape)
+    print(d.location)
     
--- gatherImages.py
+++ gatherImages.py
@@ -18,7 +18,7 @@ class VideoCapture:
 
     def __init__(self, ip):
       #Setting width and height for the camera
-      self.width, self.height = 1280, 720
+      self.width, self.height = 640, 360
       
       #Setting the device
       if ip!='':
@@ -61,6 +61,9 @@ class interactive_tool:
     #Flag to update image
     update_image=True
     
+    #Counter to check which position is being set
+    poscounter=0
+    
     #Constructor
     def __init__(self,ip=''):
         #Making tkinter window
@@ -71,6 +74,8 @@ class interactive_tool:
         
         #Init for UI elements to show on tk window
         self.UIe = {}
+        
+        self.px,self.py=0,0
 
     #To capture live from camera and show on the UI 
     def show_frame(self):
@@ -86,7 +91,7 @@ class interactive_tool:
             frame = cv2.flip(frame, 1)
             
             #Color format change
-            cv2image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)
+            cv2image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
             
             #Save latest image
             self.np_img = np.array(cv2image)
@@ -98,18 +103,83 @@ class interactive_tool:
             #Setting image to the UI element
             self.UIe['lmain'].imgtk = imgtk
             self.UIe['lmain'].configure(image=imgtk)
+        else:
+            tempimg = np.copy(self.np_img)
             
+            for i in range(0,self.poscounter):                
+                tempimg[self.tempdata.location[i,0],self.tempdata.location[i,1],:]=[255,0,0]
+                               
+                
+            #Generating image object to show
+            img = Image.fromarray(tempimg)        
+            imgtk = ImageTk.PhotoImage(image=img)
+            
+            #Setting image to the UI element
+            self.UIe['lmain'].imgtk = imgtk
+            self.UIe['lmain'].configure(image=imgtk)
+        
+        #Show the mouse coordinates on the status
+        self.statusstr = 'X:'+str(self.px)+'\tY:'+str(self.py)+'\t'
+        
+        #Show what's being set
+        if self.update_image:
+            self.statusstr += 'Pause the frame to start setting the position'
+        else:        
+            self.statusstr += 'Setting:'+data.points[self.poscounter]
+        
+        
+        #Update the status variable
+        self.status.set(self.statusstr)
+        
+        #Update next frame on UI at 30FPS
         self.UIe['lmain'].after(1000//30, self.show_frame)
-    
+           
     #To stop live video and track mouse pointer for click positions
     def start_gathering(self):
-        self.update_image = not self.update_image
-        if self.update_image:
-            self.UIe['start_button'].configure(text = "Pause frame and set posittions to save")
-        else:            
-            self.UIe['start_button'].configure(text = "Discard frame and continue live video")
         
-    
+        #Invert update image flag
+        self.update_image = not self.update_image
+        
+        #Check the update image state
+        if self.update_image:
+            #Update the button text
+            self.UIe['start_button'].configure(text = "Pause frame and set posittions to save")            
+        else:            
+            #Update the button text
+            self.UIe['start_button'].configure(text = "Discard frame and continue live video")
+            #Create new data object
+            self.tempdata=data.data()
+            self.tempdata.img=self.np_img
+     
+    #On mouse move
+    def motion(self,event):
+        #set mouse x and y; -2 because of window position
+        self.px, self.py = event.x-2, event.y-2
+     
+    #On mouse click
+    def mouse_clicked(self,event): 
+        if not self.update_image:
+            if self.poscounter >= len(data.points):
+                self.tempdata.save()
+                self.reset_data()
+            self.tempdata.location[self.poscounter,1]=self.px
+            self.tempdata.location[self.poscounter,0]=self.py
+            self.poscounter+=1
+            if self.poscounter >= len(data.points):
+                self.tempdata.save()
+                self.reset_data()
+        
+    #On reset
+    def reset_data(self,event=None):         
+        self.poscounter=0
+        self.update_image = True
+        del self.tempdata
+        self.UIe['start_button'].configure(text = "Pause frame and set positions to save")
+        
+        
+    def save_data(self):
+        pass
+        
     #Main run for tk UI
     def run(self):
         
@@ -123,9 +193,16 @@ class interactive_tool:
         self.UIe['start_button'] = tk.Button(self.win,text = "Pause frame and set posittions to save",command = self.start_gathering)
         self.UIe['start_button'].pack()
         
+        #Label to show status
+        self.status = tk.StringVar()
+        self.UIe['status'] = tk.Label(self.win,textvariable= self.status)
+        self.UIe['status'].pack()
+        self.status.set("Pause the frame to start positioning")
         
         self.show_frame()
-        
+        self.win.bind('<Motion>', self.motion)
+        self.win.bind("<Button-1>", self.mouse_clicked)
+        self.win.bind("<Button-3>", self.reset_data)
         self.win.mainloop()
         self.close()
         
